f5e24ba2a9952447017de216056e18fbffce8ae4
Jennifer Bell & Tim Kellogg <student@epicodus.com>
2015-08-25 11:32:36 -0700

    Add validation for customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,6 +1,11 @@
 class Customer < ActiveRecord::Base
   has_many :orders
 
+  validates(:firstname, {:presence => true, :length => { :maximum => 20 }})
+  validates(:lastname, {:presence => true, :length => { :maximum => 20 }})
+
+
+
   def fullname
     self.firstname + " " + self.lastname
   end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,12 +1,25 @@
 require 'spec_helper'
+require 'pry'
 
 describe Order do
 
   before { @customer = Customer.create({firstname: 'Bob', lastname: 'Bobson'}) }
-
+  before { @order = Order.create({customer_id: @customer.id, total: 650 }) }
+  before { @product  = Product.create({price: 50.00, name: 'Thingy', description: 'A thing-like thing.', order_id: @order.id})}
+  before { @product  = Product.create({price: 50.00, name: 'Stuff', description: 'A stuff-like thing.', order_id: @order.id})}
   describe 'fullname' do
     it "returns the fullname of the customer" do
       expect(@customer.fullname).to eq 'Bob Bobson'
     end
+  end
+
+  it("ensures the length of a firstname or lastname is at most 20 characters") do
+    customer = Customer.new({:firstname => "Maximilliandecemberoflcopterus", :lastname => "Smith"})
+    expect(customer.save()).to(eq(false))
+  end
+
+  it "returns false if firstname or lastname is blank" do
+    customer = Customer.new({:firstname => "", :lastname => ""})
+    expect(customer.save()).to(eq(false))
   end
 end
